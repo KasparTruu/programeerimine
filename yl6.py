86ecd0c847ef22db1665a1541ae6c349e778aad5
KasparTruu <KasparTruu69@gmail.com>
2024-06-17 12:43:28 +0300

    121

--- yl6.py
+++ yl6.py
@@ -2,11 +2,6 @@ arv1 = float(input("Sisesta esimene arv: "))
 arv2 = float(input("Sisesta teine arv: "))
 arv3 = float(input("Sisesta kolmas arv: "))
 
-if arv2 > max_arv:
-    max_arv = arv2
-
-
-if arv3 > max_arv:
-    max_arv = arv3
+max_arv = max(arv1, arv2, arv3)
 
 print("Maksimum arv on:", max_arv)
